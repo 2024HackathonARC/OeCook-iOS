a15dd8f6cd4995bdced2ae09e0772ab41518643b
“xixn2” <ssbkh0802@naver.com>
2024-10-29 16:11:29 +0900

    🎉 :: Project Setting

---
+++ Project/Sources/Application/oeCookApp.swift
@@ -0,0 +1,17 @@
+//
+//  oeCookApp.swift
+//  oeCook
+//
+//  Created by 서지완 on 10/29/24.
+//
+
+import SwiftUI
+
+@main
+struct oeCookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
